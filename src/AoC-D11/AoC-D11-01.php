c9a40bcae8b0a10f4d1d352676ef109d243031db
guillermoig <Guillermo@Mac-mini-de-Guillermo.local>
2020-12-12 02:25:13 +0100

    Day 11 done!!

---
+++ src/AoC-D11/AoC-D11-01.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once('SeatManager.php');
+
+// Main program.
+$options = getopt("f:", ['filePath:']);
+$filePath = ($options['f']) ?: $options['filePath'];
+$rows = explode("\n",file_get_contents($filePath));
+$seats = [];
+foreach ($rows as $key => $row) {
+  $seats[$key] = str_split($row);
+}
+// Solution
+$seatManager = new SeatManager($seats);
+$anyChange = TRUE;
+while ($anyChange) {
+  // $seatManager->printSeatsGrid();
+  $seatManager->assignSeats("closest");
+  $anyChange = $seatManager->getChangeAfterAssignment();
+}
+$seatManager->printSeatsGrid();
+$occupiedSeats = $seatManager->getOccupiedSeats();
+echo "\nThe number of occupied seats is: $occupiedSeats\n\n"; // 2265
+?>
